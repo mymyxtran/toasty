0a97061708f6b4f3e26fa60443e65762b1418aa3
Emily Bao <emily.yf.bao@gmail.com>
2020-11-07 11:51:32 -0700

    Default time displayed as 25:00

--- _javascript/timer.js
+++ _javascript/timer.js
@@ -1,14 +1,15 @@
 // Constants
-let workingTimeTotal = 25*60;
-let shortBreakTime = 5*60;
-let workState = -1;
-let sBreakState = -2;
-let lBreakState = -3;
-let currState = workState;
+const workingTimeTotal = 25*60;
+const shortBreakTime = 5*60;
+const workState = -1;
+const sBreakState = -2;
+const lBreakState = -3;
+
 
 // Variables
-let timeLeft = 25*60;
-let timerRunning = false;
+var timeLeft = 25*60;
+var timerRunning = false;
+var currState = workState;
 
 //Listeners
 const pomodoroTimer = document.querySelector('#pomodoro-timer');
@@ -17,8 +18,8 @@ const pauseButton = document.querySelector('#pause');
 const stopButton = document.querySelector('#stop');
 
 const displayCurrentTimeLeft = () => {
-    const seconds = timeLeft%60;
-    const minutes = Math.floor(timeLeft/60);
+    var seconds = timeLeft%60;
+    var minutes = Math.floor(timeLeft/60);
 
     function addLeadingZeros (time) {
         if (time < 10){
@@ -28,15 +29,23 @@ const displayCurrentTimeLeft = () => {
         }
     }
 
-    let result = `${addLeadingZeros(minutes)} : ${addLeadingZeros(seconds)}`;
-    document.getElementById('time-output').innerHTML = result.toString();
+    let result = `${addLeadingZeros(minutes)}:${addLeadingZeros(seconds)}`;
+    document.getElementById('time-result').innerHTML = result.toString();
 
 
 }
+
+function stopTimer(){
+    currState = workState;
+    timeLeft = workingTimeTotal;
+    timerRunning = false;
+    clearInterval(timerHandler);
+    displayCurrentTimeLeft;
+}
+
 const toggleClock = (reset) => {
     if(reset){
-        currState = workState;
-        timeLeft = workingTimeTotal;
+        stopTimer();
     }else{
         // Toggle the state of the timer
         if(timerRunning){
