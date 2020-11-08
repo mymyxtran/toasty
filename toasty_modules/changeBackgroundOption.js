065eb6316b4990c40d6eba7492244c90936448a5
mymytraan <57696924+mymytraan@users.noreply.github.com>
2020-11-08 01:30:29 -0500

    trying to get the toggle background working by toggling a class

--- toasty_modules/changeBackgroundOption.js
+++ toasty_modules/changeBackgroundOption.js
@@ -1,7 +1,20 @@
-// $(document).ready(function() {
-//     $("changeBG").click(function() {
-//       $(".background").toggleClass("active");
-//     });
-//   });
+function toggleBG (){
+    document.getElementById(toggleBG).innerHTML = "Changed Background";
+    document.body.style.backgroundImage = 'url(snowy_fest.gif)';  
+}
 
+
+// toggle bg listen
+startButton.addEventListener('click', () => {
+    toggleBG();
+}) 
+
+// var className = ' ' + myButton.className + ' ';
+
+// if ( ~className.indexOf(' active ') ) {
+//     this.className = className.replace(' active ', ' ');
+// } else {
+//     this.className += ' active';
+// }              
+// }
  
