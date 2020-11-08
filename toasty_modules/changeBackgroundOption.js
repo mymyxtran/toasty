bed6c837ab3ef0f059a2ac2b160b6d1003b9db19
mymytraan <57696924+mymytraan@users.noreply.github.com>
2020-11-08 10:11:15 -0500

    change title size

--- toasty_modules/changeBackgroundOption.js
+++ toasty_modules/changeBackgroundOption.js
@@ -1,14 +1,16 @@
+let toggleBG = document.querySelector('#toggleBG');
 
+toggleBG.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('photos/snowy_fest.gif')";
 
-function toggleBG(){
-var element = document.getElementById("background_1");
-  element.className = "background_two";
-}
+)};
+// function toggleBG(){
+// var element = document.getElementById("background_1");
+//   element.className = "background_two";
+// }
 // window.onload = function(){
 //     document.getElementById("myElement").addEventListener( 'click', toggleBG);
 // }
-
-
 
 // var className = ' ' + myButton.className + ' ';
 
