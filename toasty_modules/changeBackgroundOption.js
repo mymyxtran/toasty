302a859798c38594c14dc08319b2c262f88fe638
mymytraan <57696924+mymytraan@users.noreply.github.com>
2020-11-08 00:41:01 -0500

    pulled emily's changes and fizing togglebackground feature

--- toasty_modules/changeBackgroundOption.js
+++ toasty_modules/changeBackgroundOption.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("changeBG"); 
 var body = document.getElementsByTagName("body");	 
  
-button.addEventListener("changeBG", function () { 
-body[0].style.backgroundImage = "./photos/snow_fest.gif" 
-}); 
+	button.addEventListener("changeBG", function () { 
+		body[0].style.backgroundImage = "url('./photos/snow_fest.gif')" 
+	}); 
